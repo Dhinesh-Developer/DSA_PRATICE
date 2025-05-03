5b9d662a50a690b8501a2afde37457e4b3af1438
Dhinesh-Developer <dhineshdeveloper07@gmail.com>
2025-05-03 06:37:28 +0530

    Basis of Data type

---
+++ src/main/java/Basics/test.java
@@ -0,0 +1,34 @@
+package Basics;
+
+public class test {
+    public static void main(String[] args) {
+
+//      -128 to 127
+        byte a = 120;
+        System.out.println(a);
+
+//        short
+        short b = 12342;
+        System.out.println(b);
+
+//        int
+        int c = 807235;
+        System.out.println(c);
+
+         // float
+        float d = 87.23f;
+        System.out.println(d);
+
+        // double
+        double e = 98743.123412;
+        System.out.println(e);
+
+//        char
+        char g = 'a';
+        System.out.println(g);
+
+//        boolean
+        boolean h = true;
+        System.out.println(h);
+    }
+}
